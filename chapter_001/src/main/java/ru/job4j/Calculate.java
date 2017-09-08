eb610ef679c0fc2618f889dd008719fd51d53f8e
Sturvi <sturvi@gmail.com>
2017-09-08 11:34:25 +0300

    added chapter_002 and modified pom.xml

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -3,19 +3,19 @@ package ru.job4j;
 /** javadoc comment for class. */
 public class Calculate {
 
-/** javadoc comment for method.
-@param args you can leave empty */
+    /** javadoc comment for method.
+     @param args you can leave empty */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 
-/**
-* Method echo.
-* @param name Your name.
-* @return Echo plus your name.
-*/
-public String echo(String name) {
-    return "Echo, echo, echo : " + name;
-}
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 
 }
