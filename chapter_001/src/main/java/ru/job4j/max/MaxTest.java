6d9a45f0a4605f0b16fa895588ca5ffc17fbc043
Sturvi <sturvi@gmail.com>
2017-09-10 13:27:40 +0300

    added finding maximum among two integers

---
+++ chapter_001/src/main/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.max;
+
+public class MaxTest {
+
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+}
