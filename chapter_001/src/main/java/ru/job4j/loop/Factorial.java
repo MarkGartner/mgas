1af6c3f1f9ad826074ccd8e211ea9c9f25a65e84
Sturvi <sturvi@gmail.com>
2017-09-10 18:16:03 +0300

    added Factorial + test

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Factorial
+{
+    public int calc(int n)
+    {
+        if (n == 1 || n == 0)
+            return 1;
+        else
+            return n * calc(n - 1);
+    }
+}
