b18476187327534384ded42adb10d90f610d7ad8
Sturvi <sturvi@gmail.com>
2017-10-08 20:30:21 +0300

    added BubbleSort + class

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+public class BubbleSort
+{
+    public int[] sort(int[] paramArray)
+    {
+        int n = paramArray.length;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int counter = 0; counter < n - 1; counter++)
+            {
+                if (paramArray[counter] > paramArray[counter + 1])
+                {
+                    int temp = paramArray[counter];
+                    paramArray[counter] = paramArray[counter + 1];
+                    paramArray[counter + 1] = temp;
+                }
+            }
+        }
+        return paramArray;
+    }
+
+}
